6de3c793906e921204544b483f8b23685f123cc3
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2026-01-09 20:34:48 +0600

    initialized the project
    installed express cors dotenv and mongodb

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('SSI-Feature-Phone-Simulation is running')
+})
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`)
+})
